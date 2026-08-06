a5bee9daed0338954cc5aab4919dbd95b7148f47
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-07 00:35:36 +0700

    membuat program utama

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -36,3 +36,15 @@ void mahasiswa::cetak(){
     cout << "Nama= " << nama << endl;
 }
 
+int main(){
+    mahasiswa mhs1(102030); //hanya NIM
+    mahasiswa mhs2("andi"); //hanya Nama
+    mahasiswa mhs3(010203, "Santi");    //Hanya nim dan nama
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
