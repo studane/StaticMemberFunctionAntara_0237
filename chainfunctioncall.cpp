53eea5852be9e1d60b12b83c3a21cffc0273c292
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-07 00:09:37 +0700

    membuat chain function di dalam public

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -3,6 +3,14 @@ using namespace std;
 class buku{
     string judul;
 
- 
+    public:
+    buku setJudul(string judul){
+        this->judul = judul;
+        return *this;   //chain function
+    }
+    string getJudul(){
+        return this->judul;
+    }
+
 };
 
