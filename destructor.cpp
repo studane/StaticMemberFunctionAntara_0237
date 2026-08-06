af862c19741111b03d688f1de968a0d382e4746f
studane <zhidaneprabowoguzlend@gmail.com>
2026-08-07 02:15:10 +0700

    membuat prosedur

--- destructor.cpp
+++ destructor.cpp
@@ -26,4 +26,21 @@ angka::~angka(){ //destructor
     cout << "alamat array sudah dilepaskan" << endl;
 }
 
+void angka::cetakData()
+{
+    for (int i=1;i<=panjang; i++)
+    {
+        cout << i << " = " << arr[i] <<endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i=1;i<=panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
 
